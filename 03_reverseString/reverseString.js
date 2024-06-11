d01a1d8d14e788fd6fefe8713c54c5970b277641
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-06-11 15:05:37 +0200

    Finished the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function (szoveg) {
+  let szovegLista = szoveg.split("");
+  let i = 0;
+  let j = szovegLista.length - 1;
+  while (i < j) {
+    let csere = szovegLista[i];
+    szovegLista[i] = szovegLista[j];
+    szovegLista[j] = csere;
+    ++i;
+    --j;
+  }
+  return szovegLista.join("");
 };
 
 // Do not edit below this line
