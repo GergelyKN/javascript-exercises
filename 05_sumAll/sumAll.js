53877f18e49dfa700a91689fdc7f9f0837806207
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-06-11 16:03:13 +0200

    Finished

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  }
 
+  let nagyobb = b;
+  let kisebb = a;
+  if (a > b) {
+    nagyobb = a;
+    kisebb = b;
+  }
+
+  let n = nagyobb - kisebb + 1;
+  let sum = (n / 2) * (kisebb + nagyobb);
+  return sum;
 };
 
 // Do not edit below this line
