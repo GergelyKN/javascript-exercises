3ba9128ca5d90a8d6a105ab920231c18c3a67b15
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-06-11 14:07:37 +0200

    Finished the second exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (szoveg, hanyszor) {
+  if (hanyszor < 0) {
+    return "ERROR";
+  }
+  let result = "";
+  for (let i = 0; i < hanyszor; ++i) {
+    result += szoveg;
+  }
+  return result;
 };
 
 // Do not edit below this line
