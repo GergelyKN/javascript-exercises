912a421461934c77dbf7cb1ddfd8137f3492f7ea
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-06-18 23:39:38 +0200

    Finished working on the 8th exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (lista) {
+  const eredmeny = lista.reduce((kezdo, lepes) => {
+    return kezdo + lepes;
+  }, 0);
+  return eredmeny;
 };
 
-const multiply = function() {
-
+const multiply = function (lista) {
+  const eredmeny = lista.reduce((kezdo, szorzat) => {
+    return kezdo * szorzat;
+  }, 1);
+  return eredmeny;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  }
+
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
@@ -29,5 +39,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
