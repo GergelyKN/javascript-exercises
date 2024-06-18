8dfb0183a94b8f08b52d33589d46e084878b1ae5
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-06-18 23:47:57 +0200

    Finished working on the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const validCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const word1 = word
+    .toLowerCase()
+    .split("")
+    .filter((x) => validCharacters.includes(x))
+    .join("");
+
+  const word2 = word1.split("").reverse().join("");
+  return word1 === word2;
 };
 
+palindromes("fasz");
 // Do not edit below this line
 module.exports = palindromes;
