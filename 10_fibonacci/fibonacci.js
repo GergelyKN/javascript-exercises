ab68167b0d1985e3c93709d6374d88dd3a50a7b7
GergelyKN <nagy.gergely.koppany@gmail.com>
2024-06-18 23:54:38 +0200

    Finished working on the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  number = parseInt(number);
+  if (number === 0) {
+    return 0;
+  }
+  if (number < 0) {
+    return "OOPS";
+  }
 
+  let first = 0;
+  let second = 1;
+  let temp;
+  for (let i = 2; i <= number; i++) {
+    temp = first + second;
+    first = second;
+    second = temp;
+  }
+  return second;
 };
 
 // Do not edit below this line
